87e724b0a8ef5b18ba2bab6f53346ee072a7697d
joaolucas <jlaa>
2022-06-10 13:17:50 -0300

    a

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./test-front'));
+app.use(express.static('./dist/angular.io-example'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: '/test-front/'}),
+    res.sendFile('index.html', {root: '/dist/angular.io-example/'}),
 );
 
 // Start the app by listening on the default Heroku port
