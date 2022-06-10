cc942eefaff8275e56b7b6a2967fbd3e312d5eaa
joaolucas <jlaa>
2022-06-10 13:08:04 -0300

    desisto

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./src/test-front'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'src/test-front/'}),
-);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
