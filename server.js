831ff73fb85446f03f47a28d5726e81c2a388c3d
joaolucas <jlaa>
2022-06-10 13:05:39 -0300

    10 da sorte

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/test-front'));
+app.use(express.static('./src/test-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/test-front/'}),
+    res.sendFile('index.html', {root: 'src/test-front/'}),
 );
 
 // Start the app by listening on the default Heroku port
